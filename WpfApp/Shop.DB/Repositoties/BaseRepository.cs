27257e1d43a0293be75bb6c879fb6d8e710c65e0
alenaphilipenko <alena.philipenko@gmail.com>
2015-07-18 12:05:37 +0200

    +baseRepo

--- WpfApp/Shop.DB/Repositoties/BaseRepository.cs
+++ WpfApp/Shop.DB/Repositoties/BaseRepository.cs
@@ -5,16 +5,45 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Shop.DB.Interfaces;
+using NHibernate.Cfg;
+using NHibernate;
 
 namespace Shop.DB.Repositoties
 {
     public class BaseRepository<T> where T : IEntity
     {
-        private T[] array;
+        Configuration myConfiguration;
+        ISessionFactory mySessionFactory;
+        ISession mySession;
 
-        public void Add(T entity) { }
-        public void Delete(T entity) { }
-        public void Update(T entity) { }
-        public T FindById(int Id) { return array[Id]; }
+        public BaseRepository()
+        {
+            myConfiguration = new Configuration();
+            myConfiguration.Configure();
+            mySessionFactory = myConfiguration.BuildSessionFactory();
+            mySession = mySessionFactory.OpenSession();
+        }
+
+        public void Add(T entity)
+        {
+            mySession.Save(entity);
+            mySession.Transaction.Commit();
+        }
+
+        public void Delete(T entity) 
+        {
+            mySession.Delete(entity);
+        }
+
+        public void Update(T entity)
+        {
+            mySession.Flush();
+            mySession.Update(entity);
+        }
+
+        public T FindById(int Id)
+        {
+            return mySession.Get<T>(Id);
+        }
     }
 }
