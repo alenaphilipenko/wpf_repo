f62313bf01a7db12c4898275d5a1d0ebf71a2c6e
artyom.malyonkin <Artyom.Malyonkin@ciber.com>
2015-07-20 12:43:39 +0200

    Revert "Merge branch 'add-repositories-and-simpleinjector'"
    This reverts commit 85d69dc65266a983c4f93e1ae84ed51d9e6be58e, reversing
    changes made to 4a3e8536a51c5234b6d1acfcaded9f7ccbe437d5.

---
+++ WpfApp/Shop.DB/Class1.cs
@@ -0,0 +1,34 @@
+﻿using NHibernate;
+using NHibernate.Cfg;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.DB
+{
+    public class Class1
+    {
+        private Configuration myConfiguration;
+        private ISessionFactory mySessionFactory;
+        private ISession mySession;
+
+        public Class1()
+        {
+            myConfiguration = new Configuration();
+            myConfiguration.Configure();
+            mySessionFactory = myConfiguration.BuildSessionFactory();
+            mySession = mySessionFactory.OpenSession();
+
+            // Add Record
+            using (mySession.BeginTransaction())
+            {
+                Entities.User loContact = new Entities.User { FirstName = "John", LastName = "Doe", IsAdmin = true};
+                mySession.Save(loContact);
+
+                mySession.Transaction.Commit();
+            }
+        }
+    }
+}
